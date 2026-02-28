cf6c38278f95efd6feb6c5a08f610f1d8e89a900
mendonca-milla <camilla_som@hotmail.com>
2026-02-28 16:41:08 -0300

    Create aula3.py
    Tipos de string

---
+++ aula3.py
@@ -0,0 +1,57 @@
+"""
+Docstring (
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Camilla Mendonça')
+print(1,'Camilla "Mendonça"')
+
+# Aspas duplas
+print("Camilla Mendonça")
+print(2, "Camilla 'Mendonça'")
+
+# Escape
+print("Camilla \"Mendonça\"")
+
+# r
+print(r"Camilla \"Mendonça\"")
+
+
+"""
+Explicação:
+
+dinamica - o py identifica automaticamente
+
+print(1234) numero inteiro
+print('Camilla') string
+
+# Escape
+print("Camilla \"Mendonça\"")
+
+Camilla "Mendonça"
+
+# r utilizo para mostrar o caractere de escape
+print(r"Camilla \"Mendonça\"")
+
+Camilla \"Mendonça\"
+
+Se quiser mostrar a aspas dentro do texto e não precisar usar escape e nem r, pode iniciar a string com aspas simples e identificar a outra com aspas dupla (ou o inverso " ' ' ") 
+
+# Aspas simples
+print('Camilla Mendonça')
+print(1,'Camilla "Mendonça"')
+
+1 Camilla "Mendonça"
+
+# Aspas duplas
+print("Camilla Mendonça")
+print(2, "Camilla 'Mendonça'")
+
+2 Camilla 'Mendonça'
+
+"""
