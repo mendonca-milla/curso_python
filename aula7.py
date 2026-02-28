313877e753ed5d5274e959dce42908bd9069982b
mendonca-milla <camilla_som@hotmail.com>
2026-02-28 19:37:11 -0300

    update aula7.py
    INTRODUÇÃO AS VARIÁVEIS EM PYTHON
    nome
    idade
    soma

--- aula7.py
+++ aula7.py
@@ -4,11 +4,11 @@
 # atribuir um valor a um nome (variável).
 # Uso: nome_variavel = expressão (snack case)
 
-# nome_completo = 'Camilla Mendonça'
-# soma_dois_mais_dois = 2 + 2
-# int_um = int('1')
-# print(int('1'), type(int_um)) # print(int('1'), type(int('1'))) pra não ficar refetindo int 1
-# print(nome_completo, soma_dois_mais_dois) # Camilla Mendonça 4
+nome_completo = 'Camilla Mendonça'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+print(int('1'), type(int_um)) # print(int('1'), type(int('1'))) pra não ficar refetindo int 1
+print(nome_completo, soma_dois_mais_dois) # Camilla Mendonça 4
 
 nome = 'Camilla'
 idade = 28
