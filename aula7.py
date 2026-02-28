c38b6e39ec64de59d5bea0f8ed1d8d0960849952
mendonca-milla <camilla_som@hotmail.com>
2026-02-28 19:35:57 -0300

    aula7.py
    INTRODUÇÃO AS VARIÁVEIS EM PYTHON

---
+++ aula7.py
@@ -0,0 +1,17 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão (snack case)
+
+# nome_completo = 'Camilla Mendonça'
+# soma_dois_mais_dois = 2 + 2
+# int_um = int('1')
+# print(int('1'), type(int_um)) # print(int('1'), type(int('1'))) pra não ficar refetindo int 1
+# print(nome_completo, soma_dois_mais_dois) # Camilla Mendonça 4
+
+nome = 'Camilla'
+idade = 28
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade) # Nome: Camilla Idade: 28
+print('É maior?', maior_de_idade) # True
