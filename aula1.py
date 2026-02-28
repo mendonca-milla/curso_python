660ff3d3ebe24933f8e9631e12dc1d5750372dcd
mendonca-milla <camilla_som@hotmail.com>
2026-02-28 14:59:21 -0300

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,17 @@
+"""
+DocString - Não é comentário e não é python mas o interpretador do phyton le
+
+Pode escrever o que 
+quiser em quantas linhas quiser
+
+Pode usar aspas duplas ou simples para abrir e fechar - escrever dentro
+
+"""
+'''
+Usar para escrever suas notas, explicar seus códigos
+
+'''
+# COMENTÁRIO DE CÓDIGOS - Permite escrever um comentário
+print(123) # Na frente (pode ser utilizada para dizer essa linha faz isso)
+# abaixo
+print(456)
