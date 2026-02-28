81eede0cc91f49dbbad5b87a5260057e897a3cea
mendonca-milla <camilla_som@hotmail.com>
2026-02-28 17:34:05 -0300

    aula5.py
    tipo de dado boolean

--- aula5.py
+++ aula5.py
@@ -5,7 +5,7 @@
 # Dentre eles o ==, que é um operador lógico que questiona se um valor é igual a outro
 print(10 == 10) # Sim => True (verdadeiro)
 print(10 == 11) # Não => False (Falso)
-print(type(True)) 
-print(type(False)) 
-print(type(10 == 10)) 
-print(type(10 == 11)) 
+print(type(True)) # <class 'bool'>
+print(type(False)) # <class 'bool'>
+print(type(10 == 10)) # <class 'bool'>
+print(type(10 == 11)) # <class 'bool'>
