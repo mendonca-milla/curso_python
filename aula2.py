296fbb4f7c4cf203a4f0df6f47df6515f99ef56c
mendonca-milla <camilla_som@hotmail.com>
2026-02-28 15:50:38 -0300

    update aula2.py
    função print - explicação

--- aula2.py
+++ aula2.py
@@ -2,3 +2,17 @@
 print(12, 34, 1011, sep='-', end='#\n')
 print(56, 78, sep="-", end='\n')
 print(9, 10, sep="-", end='\n')
+
+"""
+função print exibe alguma coisa na tela para o output
+
+toda função precisa de ()
+
+print(12,34) argumento não nomeados
+
+argumento não nomeados 12, 34
+
+sep = separador ' - '
+
+
+"""
