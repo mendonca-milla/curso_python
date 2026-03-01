2e86d8b362c8fb339e815b715ebfe2d70e459d77
mendonca-milla <camilla_som@hotmail.com>
2026-03-01 16:04:35 -0300

    aula9.py
    INTRODUÇÃO OPERADORES ARITMÉTICOS

---
+++ aula9.py
@@ -0,0 +1,40 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2 #float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3 # corta das casas decimais
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 4 # um numero elevado à outro numero
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão %
+print('Módulo', modulo)
+
+# Adição 20
+# Subtração 5
+# Multiplicação 100
+# Divisão 5.0
+# Divisão inteira 3
+# Exponenciação 16
+# Módulo 1
+
+print(10 % 8 == 0) # False 10 não é divisível por 8
+print(16 % 8 == 0) # True
+print(10 % 2 == 0) # True
+print(15 % 2 == 0) # False
+print(16 % 2 == 0) # True
+
+# P/ descobrir se um numero é par ou impar pode perguntar se ele é divisível por 2
+
+# print(10 % 2 == 0) True
+# print(15 % 2 == 0) False
+# print(16 % 2 == 0) True
