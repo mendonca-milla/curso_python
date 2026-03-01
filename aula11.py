1664037af4063bad2bbc7f4bd86ec55bf49d3c2e
mendonca-milla <camilla_som@hotmail.com>
2026-03-01 16:26:40 -0300

    aula11.py
    PRECEDENCIA ENTRE OS OPERADORES ARITMÉTICOS

---
+++ aula11.py
@@ -0,0 +1,10 @@
+# PRECEDENCIA ENTRE OS OPERADORES ARITMÉTICOS
+
+# Ordem de execução da conta:
+# 1. (n + n) dentro dos parenteses
+# 2. ** exponenciação
+# 3. * / // % multiplicação, divisão, divisão inteira e módulo
+# 4. + - adição e subtração
+
+conta_1 = (1 + 1) ** (5 + 5) # 1024
+print(conta_1)
