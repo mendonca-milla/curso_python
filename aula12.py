201052abe353785d608192f72320993f41bc29fc
mendonca-milla <camilla_som@hotmail.com>
2026-03-01 16:46:12 -0300

    aula12.py
    EXERCÍCIO CÁLCULO IMC

---
+++ aula12.py
@@ -0,0 +1,14 @@
+# EXERCÍCIO CÁLCULO IMC + ELLIPSIS
+
+nome = 'Camilla' 
+altura = 1.60
+peso =  58
+imc = 58 / (1.60 * 1.60) # IMC = peso / (altura x altura)
+
+print(nome, 'tem', altura, 'de altura')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
+
+# Camilla tem 1.6 de altura
+# pesa 58 quilos e seu IMC é
+# 22.656249999999996
