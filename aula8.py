6e1b429cee31d101b99a763a4ba6a964af028bce
mendonca-milla <camilla_som@hotmail.com>
2026-03-01 15:37:13 -0300

    aula8.py
    EXERCÍCIO COM VARIÁVEIS E TIPO DE DADOS

---
+++ aula8.py
@@ -0,0 +1,24 @@
+nome = 'Camilla'
+sobrenome = 'Mendonça'
+idade = 27
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.60 
+
+
+print ('Nome:', nome)
+print ('sobrenome:', sobrenome)
+print ('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade:', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
+"""
+Nome: Camilla
+sobrenome: Mendonça
+Idade: 27
+Ano de nascimento: 1998
+É maior de idade: True
+Altura em metros: 1.6
+
+"""
