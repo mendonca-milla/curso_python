3d1c167b8a22a8d4a09dfddce58d38f6b8a32b91
mendonca-milla <camilla_som@hotmail.com>
2026-03-01 16:15:05 -0300

    aula10.py
    CONCATENAÇÃO (+) E REPETIÇÃO (*) COM OPERADORES ARITMÉTICOS

---
+++ aula10.py
@@ -0,0 +1,16 @@
+#CONCATENAÇÃO (+) E REPETIÇÃO (*) COM OPERADORES ARITMÉTICOS
+
+concatenacao = 'Camilla' + ' ' + 'Mendonça'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_camilla = 3 * 'Camilla' 
+print(a_dez_vezes)
+print(tres_vezes_camilla)
+
+# o sinal de + em string faz a concatenação
+# o sinal de * em string ou string com um int faz a repetição
+
+# Camilla Mendonça
+# AAAAAAAAAA
+# CamillaCamillaCamilla
